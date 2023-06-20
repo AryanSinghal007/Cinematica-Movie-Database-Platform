9212810b5854c9ef4f98aff4194815852f2f38cf
Aryan Singhal <aryansinghal237@gmail.com>
2023-06-20 13:50:36 +0530

    Update commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Layout } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import Homepage from "./components/Homepage";
+import Movies from "./components/Movies";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+
+const { Content } = Layout;
+
+const App = () => {
+    return (
+
+        <Router>
+            <div>
+                <Navbar />
+            </div>
+            <div>
+                <Layout >
+                    <Content>
+                        <div >
+                            <Routes>
+                                <Route path="/" element={<Homepage />} />
+                                <Route path="/movies" element={<Movies />} />
+                                <Route path="/about" element={<About />} />
+                                <Route path="/contact" element={<Contact />} />
+                            </Routes>
+                        </div>
+                    </Content>
+                </Layout>
+            </div>
+            <div>
+                <Footer />
+            </div>
+        </Router>
+    );
+};
 
 export default App;
